ee908366e860284ba59367aedeee72222132edcc
dinghe <308024784@qq.com>
2021-04-07 21:15:38 +0800

    feat - support transaction

--- geeorm_test.go
+++ geeorm_test.go
@@ -1,10 +1,17 @@
 package geeorm
 
 import (
+	"errors"
+	"geeorm/session"
 	"testing"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+type User struct {
+	Name string `geeorm:"PRIMARY KEY"`
+	Age  int
+}
 
 func OpenDB(t *testing.T) *Engine {
 	t.Helper()
@@ -18,4 +25,45 @@ func OpenDB(t *testing.T) *Engine {
 func TestNewEngine(t *testing.T) {
 	e := OpenDB(t)
 	defer e.Close()
+}
+
+func TestEngine_Transaction(t *testing.T) {
+	t.Run("rollback", func(t *testing.T) {
+		transactionRollback(t)
+	})
+	t.Run("commit", func(t *testing.T) {
+		transactionCommit(t)
+	})
+}
+
+func transactionRollback(t *testing.T) {
+	e := OpenDB(t)
+	defer e.Close()
+	s := e.NewSession()
+	_ = s.Model(&User{}).DropTable()
+	_, err := e.Transaction(func(s *session.Session) (interface{}, error) {
+		_ = s.Model(&User{}).CreateTable()
+		_, _ = s.Insert(&User{"Tom", 18})
+		return nil, errors.New("Error")
+	})
+	if err == nil || s.HasTable() {
+		t.Fatal("failed to rollback")
+	}
+}
+
+func transactionCommit(t *testing.T) {
+	e := OpenDB(t)
+	defer e.Close()
+	s := e.NewSession()
+	_ = s.Model(&User{}).DropTable()
+	_, err := e.Transaction(func(s *session.Session) (interface{}, error) {
+		_ = s.Model(&User{}).CreateTable()
+		_, _ = s.Insert(&User{"Tom", 18})
+		return nil, nil
+	})
+	u := &User{}
+	_ = s.First(u)
+	if err != nil || u.Name != "Tom" {
+		t.Fatal("failed to commit")
+	}
 }
